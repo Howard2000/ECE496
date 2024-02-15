bdbee60791dd252142911518065231aad2d98e24
Zhu Hao <zhuhao0324@gmail.com>
2024-02-15 16:27:22 -0500

    added trainer

--- main.py
+++ main.py
@@ -2,13 +2,15 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import matplotlib.pyplot as plt # for plotting
+import torch.optim as optim #for gradient descent
 
 torch.manual_seed(1) # set the random seed
 
 #Convolutional Neural Network Architecture
-class CNN_MNISTClassifier(nn.Module):
+class CNN_Classifier(nn.Module):
     def __init__(self):
-        super(CNN_MNISTClassifier, self).__init__()
+        super(CNN_Classifier, self).__init__()
         self.conv1 = nn.Conv2d(3, 64, 8) #in_channels, out_chanels, kernel_size
         self.pool = nn.MaxPool2d(2, 2) #kernel_size, stride 
         self.conv2 = nn.Conv2d(64, 64, 4) #in_channels, out_chanels, kernel_size
@@ -28,9 +30,9 @@ class CNN_MNISTClassifier(nn.Module):
     
     print('Convolutional Neural Network Architecture Done')
 
-class CNN_MNISTClassifier_2(nn.Module):
+class CNN_Classifier_2(nn.Module):
     def __init__(self):
-        super(CNN_MNISTClassifier, self).__init__()
+        super(CNN_Classifier_2, self).__init__()
         self.conv1 = nn.Conv2d(3, 64, 8) #in_channels, out_chanels, kernel_size
         self.pool = nn.MaxPool2d(2, 2) #kernel_size, stride 
         self.conv2 = nn.Conv2d(64, 64, 4) #in_channels, out_chanels, kernel_size
@@ -54,7 +56,69 @@ class CNN_MNISTClassifier_2(nn.Module):
     
     print('Convolutional Neural Network Architecture Done')
 
+def get_accuracy(model, train=False):
+    if train:
+        data = rat_train
+    else:
+        data = rat_val
+
+    correct = 0
+    total = 0
+    for imgs, labels in torch.utils.data.DataLoader(data, batch_size=64):
+
+        output = model(imgs)
+
+        #select index with maximum prediction score
+        pred = output.max(1, keepdim=True)[1]
+        correct += pred.eq(labels.view_as(pred)).sum().item()
+        total += imgs.shape[0]
+    return correct / total
+
+def train(model, data, batch_size=64, num_epochs=1):
+    train_loader = torch.utils.data.DataLoader(data, batch_size=batch_size)
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.SGD(model.parameters(), lr=0.01, momentum=0.9)
+
+    iters, losses, train_acc, val_acc = [], [], [], []
+
+    # training
+    n = 0 # the number of iterations
+    for epoch in range(num_epochs):
+        for imgs, labels in iter(train_loader):
+
+            out = model(imgs)             # forward pass
+
+            loss = criterion(out, labels) # compute the total loss
+            loss.backward()               # backward pass (compute parameter updates)
+            optimizer.step()              # make the updates for each parameter
+            optimizer.zero_grad()         # a clean up step for PyTorch
+
+            # save the current training information
+            iters.append(n)
+            losses.append(float(loss)/batch_size)             # compute *average* loss
+            train_acc.append(get_accuracy(model, train=True)) # compute training accuracy
+            val_acc.append(get_accuracy(model, train=False))  # compute validation accuracy
+            n += 1
+
+    # plotting
+    plt.title("Training Curve")
+    plt.plot(iters, losses, label="Train")
+    plt.xlabel("Iterations")
+    plt.ylabel("Loss")
+    plt.show()
+
+    plt.title("Training Curve")
+    plt.plot(iters, train_acc, label="Train")
+    plt.plot(iters, val_acc, label="Validation")
+    plt.xlabel("Iterations")
+    plt.ylabel("Training Accuracy")
+    plt.legend(loc='best')
+    plt.show()
+
+    print("Final Training Accuracy: {}".format(train_acc[-1]))
+    print("Final Validation Accuracy: {}".format(val_acc[-1]))
+
 if __name__ == '__main__':
     print("CNN")
-    model = CNN_MNISTClassifier()
+    model = CNN_Classifier()
     print(model)
