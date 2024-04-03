5cc3fbea833a9208c808f9d3d002e93875b5b029
Pablo Medina Lara <medinalarapablo@gmail.com>
2024-04-02 23:36:56 -0400

    Fully working training

--- main.py
+++ main.py
@@ -18,9 +18,13 @@ class RatDataset(Dataset):
             for j in range (7):
                 if train:
                     train_data = torch.tensor(fold[i][j]['training_data_rat']).transpose(1,0)
+                    # print(f"train_Data shape: {train_data.size()}")
+
                     self.data = torch.cat((self.data, train_data), 0)
 
                     train_lables = torch.tensor(fold[i][j]['training_data_labels']).squeeze(1)
+                    # print(f"train_lables.shape: {train_lables.size()}")
+   
                     train_lables = torch.sub(train_lables, torch.ones_like(train_lables))
                     self.labels = torch.cat((self.labels, train_lables), 0)
 
@@ -33,6 +37,11 @@ class RatDataset(Dataset):
 
                     self.labels = torch.cat((self.labels, test_labels), 0)
         print("Data done")
+        idx = torch.randperm(self.data.shape[0])
+        self.data = self.data[idx].view(self.data.size())
+        self.labels = self.labels[idx].view(self.labels.size())
+
+        print(f"shape: {self.data.size()} lables: {self.labels.size()}")
 
     def __len__(self):
         return torch.numel(self.labels)
@@ -51,23 +60,19 @@ class CNN_Classifier(nn.Module):
         self.conv1 = nn.Conv2d(1, 64, 8) #in_channels, out_chanels, kernel_size
         self.pool = nn.MaxPool2d(2, 2) #kernel_size, stride 
         self.conv2 = nn.Conv2d(64, 64, 4) #in_channels, out_chanels, kernel_size
-        self.pool2 = nn.MaxPool2d(2, 2) #kernel_size, stride 
         self.conv3 = nn.Conv2d(64, 64, 2) #in_channels, out_chanels, kernel_size
-        self.pool3 = nn.MaxPool2d(2, 2) #kernel_size, stride 
         self.fc1 = nn.Linear(2560, 256)
-        self.fc2 = nn.Linear(256, 64)
-        self.fc3 = nn.Linear(64, 3)
+        self.fc2 = nn.Linear(256, 3)
         print('Convolutional Neural Network Architecture Done')
 
 
     def forward(self, x):
         x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool2(F.relu(self.conv2(x)))
-        x = self.pool3(F.relu(self.conv3(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = self.pool(F.relu(self.conv3(x)))
         x = x.view(x.size(0), -1)
         x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = self.fc3(x)
+        x = self.fc2(x)
         return x
     
 
@@ -102,15 +107,25 @@ def get_accuracy(model, data):
     kwargs = {'num_workers': 2, 'pin_memory': False}
     correct = 0
     total = 0
+    n = 0
     for imgs, labels in torch.utils.data.DataLoader(data, batch_size=64, **kwargs):
         imgs = imgs.to(device)
         labels = labels.to(device)
-        output = model(torch.reshape(imgs, (-1,1,100,56)))
+        output = model(torch.reshape(imgs, (-1,1,56,100)))
 
         #select index with maximum prediction score
         pred = output.max(1, keepdim=True)[1]
         correct += pred.eq(labels.view_as(pred)).sum().item()
         total += imgs.shape[0]
+        # if (n == 0):
+        #     print(f"output shape: {output.size()} pred shape: {pred.size()} lables: {labels.size()}")
+        #     print("Output: ")
+        #     print(output[0:10])
+        #     print("Pred: ")
+        #     print(pred[0:10])
+        #     print("lables :")
+        #     print(labels[0:10])
+        n += 1
     return correct / total
 
 def train(model, data, batch_size=64, num_epochs=1):
@@ -121,20 +136,20 @@ def train(model, data, batch_size=64, num_epochs=1):
     test_data = RatDataset(False)
     # test_loader = torch.utils.data.DataLoader(test_data, batch_size = batch_size)
     criterion = nn.CrossEntropyLoss()
-    optimizer = optim.SGD(model.parameters(), lr=0.01, momentum=0.9)
-
+    optimizer = optim.SGD(model.parameters(), lr=0.005, momentum=0.8)
+    train_acc = []
+    val_acc = []
     iters, losses, train_acc, val_acc = [], [], [], []
 
     # training
     n = 0 # the number of iterations
     for epoch in range(num_epochs):
         for imgs, labels in iter(train_loader):
-            if (n % 500 == 0 ):
-                print(f"TRaining {n}")
+            
             imgs = imgs.to(device)
             labels = labels.to(device)
             optimizer.zero_grad()         # a clean up step for PyTorch
-            out = model(torch.reshape(imgs, (-1,1,100,56))).to(device)         
+            out = model(torch.reshape(imgs, (-1,1,56,100))).to(device)         
             labels = labels.type(torch.LongTensor).to(device)    # forward pass
 
             loss = criterion(out, labels) # compute the total loss
@@ -143,28 +158,35 @@ def train(model, data, batch_size=64, num_epochs=1):
             
 
             # save the current training information
-            iters.append(n)
+            
             losses.append(float(loss)/batch_size)             # compute *average* loss
-            train_acc.append(get_accuracy(model, train=True))
-            val_acc.append(get_accuracy(model, train=False))
-
+            if (n % 2000 == 0 ):
+                print(f"TRaining {n}")
+                # print(f"out.shape: {out.size()}")
+                # print(f"lables shape: {labels.size()}")
+            
             n += 1
-        print("Epoch {} Training Accuracy: {}".format(epoch, get_accuracy(model, data)))
-        print("Epoch {} Validation Accuracy: {}".format(epoch, get_accuracy(model, test_data)))
+        iters.append(epoch)
+        train = get_accuracy(model, data)
+        val = get_accuracy(model, test_data)
+        train_acc.append(train)
+        val_acc.append(val)
+        print("Epoch {} Training Accuracy: {}".format(epoch, train))
+        print("Epoch {} Validation Accuracy: {}".format(epoch, val))
     # plotting
     plt.title("Training Curve")
-    plt.plot(iters, losses, label="Train")
+    plt.plot(losses, label="Train")
     plt.xlabel("Iterations")
     plt.ylabel("Loss")
-    plt.show()
-
-    plt.title("Training Curve")
+    
+    plt.title("Acc Curve")
     plt.plot(iters, train_acc, label="Train")
-    plt.plot(iters, val_acc, label="Validation")
+    plt.plot(iters, val_acc, label="Test")
     plt.xlabel("Iterations")
-    plt.ylabel("Training Accuracy")
+    plt.ylabel("Accuracy")
     plt.legend(loc='best')
     plt.show()
+
 
 
     print("Final Training Accuracy: {}".format(get_accuracy(model, data)))
@@ -174,34 +196,35 @@ if __name__ == '__main__':
     torch.manual_seed(1) # set the random seed
     fold = [[],[],[]]
 
-    fold[0].append(mat4py.loadmat('./data/Rat4Training_Fold1.mat'))
-    # fold[0].append(mat4py.loadmat('C:/Users/pablo/Documents/Capstone/7x8/Rat5Training_Fold1.mat'))
-    # fold[0].append(mat4py.loadmat('C:/Users/pablo/Documents/Capstone/7x8/Rat6Training_Fold1.mat'))
-    # fold[0].append(mat4py.loadmat('C:/Users/pablo/Documents/Capstone/7x8/Rat7Training_Fold1.mat'))
-    # fold[0].append(mat4py.loadmat('C:/Users/pablo/Documents/Capstone/7x8/Rat8Training_Fold1.mat'))
-    # fold[0].append(mat4py.loadmat('C:/Users/pablo/Documents/Capstone/7x8/Rat9Training_Fold1.mat'))
-    # fold[0].append(mat4py.loadmat('C:/Users/pablo/Documents/Capstone/7x8/Rat10Training_Fold1.mat'))
+    fold[0].append(mat4py.loadmat('C:/Users/pablo/Documents/Capstone/7x8/Rat4Training_Fold1.mat'))
+    fold[0].append(mat4py.loadmat('C:/Users/pablo/Documents/Capstone/7x8/Rat5Training_Fold1.mat'))
+    fold[0].append(mat4py.loadmat('C:/Users/pablo/Documents/Capstone/7x8/Rat6Training_Fold1.mat'))
+    fold[0].append(mat4py.loadmat('C:/Users/pablo/Documents/Capstone/7x8/Rat7Training_Fold1.mat'))
+    fold[0].append(mat4py.loadmat('C:/Users/pablo/Documents/Capstone/7x8/Rat8Training_Fold1.mat'))
+    fold[0].append(mat4py.loadmat('C:/Users/pablo/Documents/Capstone/7x8/Rat9Training_Fold1.mat'))
+    fold[0].append(mat4py.loadmat('C:/Users/pablo/Documents/Capstone/7x8/Rat10Training_Fold1.mat'))
     print("Done ")
-    fold[1].append(mat4py.loadmat('./data/Rat4Training_Fold2.mat'))
-    # fold[1].append(mat4py.loadmat('C:/Users/pablo/Documents/Capstone/7x8/Rat5Training_Fold2.mat'))
-    # fold[1].append(mat4py.loadmat('C:/Users/pablo/Documents/Capstone/7x8/Rat6Training_Fold2.mat'))
-    # fold[1].append(mat4py.loadmat('C:/Users/pablo/Documents/Capstone/7x8/Rat7Training_Fold2.mat'))
-    # fold[1].append(mat4py.loadmat('C:/Users/pablo/Documents/Capstone/7x8/Rat8Training_Fold2.mat'))
-    # fold[1].append(mat4py.loadmat('C:/Users/pablo/Documents/Capstone/7x8/Rat9Training_Fold2.mat'))
-    # fold[1].append(mat4py.loadmat('C:/Users/pablo/Documents/Capstone/7x8/Rat10Training_Fold2.mat'))
+    fold[1].append(mat4py.loadmat('C:/Users/pablo/Documents/Capstone/7x8/Rat4Training_Fold2.mat'))
+    fold[1].append(mat4py.loadmat('C:/Users/pablo/Documents/Capstone/7x8/Rat5Training_Fold2.mat'))
+    fold[1].append(mat4py.loadmat('C:/Users/pablo/Documents/Capstone/7x8/Rat6Training_Fold2.mat'))
+    fold[1].append(mat4py.loadmat('C:/Users/pablo/Documents/Capstone/7x8/Rat7Training_Fold2.mat'))
+    fold[1].append(mat4py.loadmat('C:/Users/pablo/Documents/Capstone/7x8/Rat8Training_Fold2.mat'))
+    fold[1].append(mat4py.loadmat('C:/Users/pablo/Documents/Capstone/7x8/Rat9Training_Fold2.mat'))
+    fold[1].append(mat4py.loadmat('C:/Users/pablo/Documents/Capstone/7x8/Rat10Training_Fold2.mat'))
     print("Done ")
-    # fold[2].append(mat4py.loadmat('./data/Rat4Training_Fold3.mat'))
-    # fold[2].append(mat4py.loadmat('C:/Users/pablo/Documents/Capstone/7x8/Rat5Training_Fold3.mat'))
-    # fold[2].append(mat4py.loadmat('C:/Users/pablo/Documents/Capstone/7x8/Rat6Training_Fold3.mat'))
-    # fold[2].append(mat4py.loadmat('C:/Users/pablo/Documents/Capstone/7x8/Rat7Training_Fold3.mat'))
-    # fold[2].append(mat4py.loadmat('C:/Users/pablo/Documents/Capstone/7x8/Rat8Training_Fold3.mat'))
-    # fold[2].append(mat4py.loadmat('C:/Users/pablo/Documents/Capstone/7x8/Rat9Training_Fold3.mat'))
-    # fold[2].append(mat4py.loadmat('C:/Users/pablo/Documents/Capstone/7x8/Rat10Training_Fold3.mat'))
+    fold[2].append(mat4py.loadmat('C:/Users/pablo/Documents/Capstone/7x8/Rat4Training_Fold3.mat'))
+    fold[2].append(mat4py.loadmat('C:/Users/pablo/Documents/Capstone/7x8/Rat5Training_Fold3.mat'))
+    fold[2].append(mat4py.loadmat('C:/Users/pablo/Documents/Capstone/7x8/Rat6Training_Fold3.mat'))
+    fold[2].append(mat4py.loadmat('C:/Users/pablo/Documents/Capstone/7x8/Rat7Training_Fold3.mat'))
+    fold[2].append(mat4py.loadmat('C:/Users/pablo/Documents/Capstone/7x8/Rat8Training_Fold3.mat'))
+    fold[2].append(mat4py.loadmat('C:/Users/pablo/Documents/Capstone/7x8/Rat9Training_Fold3.mat'))
+    fold[2].append(mat4py.loadmat('C:/Users/pablo/Documents/Capstone/7x8/Rat10Training_Fold3.mat'))
+    print(len(fold[0][0]))
     print("Done ")
     device = torch.device('cuda:0')
     print("CNN")
     model = CNN_Classifier().to(device)
     data = RatDataset(train = True)
-    train(model, data, num_epochs=100)
+    train(model, data, num_epochs=50)
     # print(model)
 # 
